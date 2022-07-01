5a4ca307786f3ddbfca708280b3249c48a4b1be9
Alexey Orlov <a.orlov@reg-is.ru>
2022-07-01 23:53:00 +0700

    Экшены авторизации клиента

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,39 @@
 class ApplicationController < ActionController::API
+  before_action :authorized
+
+  def encode_token(payload)
+    JWT.encode(payload, ENV['SECRET'])
+  end
+
+  def auth_header
+    # { Authorization: 'Bearer <token>' }
+    request.headers['Authorization']
+  end
+
+  def decoded_token
+    if auth_header
+      token = auth_header.split(' ')[1]
+      # header: { 'Authorization': 'Bearer <token>' }
+      begin
+        JWT.decode(token, ENV['SECRET'], true, algorithm: 'HS256')
+      rescue JWT::DecodeError
+        nil
+      end
+    end
+  end
+
+  def logged_in_client
+    if decoded_token
+      client_id = decoded_token[0]['client_id']
+      @client = Client.find_by(id: client_id)
+    end
+  end
+
+  def logged_in?
+    !!logged_in_client
+  end
+
+  def authorized
+    render json: { message: 'Пожалуйста, войдите в систему' }, status: :unauthorized unless logged_in?
+  end
 end
