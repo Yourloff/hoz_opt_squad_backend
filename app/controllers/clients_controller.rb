4f36891e4cae23a0bd082b27eb1b227791234e3b
Alexey Orlov <a.orlov@reg-is.ru>
2022-07-02 11:41:21 +0700

    Исправлен params client

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -5,10 +5,10 @@ class ClientsController < ApplicationController
   def create
     @client = Client.create(client_params)
     if @client.valid?
-      token = encode_token({client_id: @client.id})
-      render json: {client: @client, token: token}
+      token = encode_token({ client_id: @client.id })
+      render json: { client: @client, token: token }
     else
-      render json: {error: "Неверное имя пользователя или пароль"}
+      render json: { error: "Неверное имя пользователя или пароль" }
     end
   end
 
@@ -17,13 +17,12 @@ class ClientsController < ApplicationController
     @client = Client.find_by(username: params[:username])
 
     if @client && @client.authenticate(params[:password])
-      token = encode_token({client_id: @client.id})
-      render json: {client: @client, token: token}
+      token = encode_token({ client_id: @client.id })
+      render json: { client: @client, token: token }
     else
-      render json: {error: "Неверное имя пользователя или пароль"}
+      render json: { error: "Неверное имя пользователя или пароль" }
     end
   end
-
 
   def auto_login
     render json: @client
@@ -31,7 +30,7 @@ class ClientsController < ApplicationController
 
   private
 
-  def user_params
-    params.permit(:username, :password, :age)
+  def client_params
+    params.permit(:username, :password)
   end
 end
