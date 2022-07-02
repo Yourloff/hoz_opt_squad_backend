5844d4ef8848a4c679f45231697dc8b0cbac44ff
Alexey Orlov <a.orlov@reg-is.ru>
2022-07-02 17:25:43 +0700

    Настроены роуты

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   resources :orders
   resource :clients, only: [:create]
-
   post '/login', to: 'clients#login'
   get '/auto_login', to: 'clients#auto_login'
+
+  resources :categories do
+    resources :subcategories
+  end
+
+  resources :products
 end
